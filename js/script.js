f14ca2426b3aecd685c3e0f2360f31657f361372
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-08-20 16:08:22 +0300

    text: datatypes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'strict mode';
+
+
+// let user = 'John', age = 25, message = 'Hello';
+// console.log(user, age, message);
+
+num = 5;
+
+const myBirthday = '10.11.1990';
+
+// const myBirthday = '10.11.1991';
+console.log(myBirthday);
+
