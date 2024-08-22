dc848aadae85ebc0040b75411c3d6dd460b8a80b
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-08-22 19:05:32 +0300

    feat: add operators

--- js/script.js
+++ js/script.js
@@ -1,42 +1,119 @@
 'strict mode';
+/*
+if (true) {
+  console.log('yes');
+} else {
+  console.log('no');
+}
+num = '65';
+
+(num === 65) ? console.log('DA!') : console.log('no');
+
+let num = +prompt('Enter number', '5');
+
+if (num % 2 == 0) {
+  console.log('Even');
+}
+else {
+  console.log('Odd');
+}
+
+console.log(num);
+let hour = 6;
+
+if (hour >= 6 && hour <= 12) {
+  console.log('Good morning');
+} else if (hour > 12 && hour <= 18) {
+  console.log('Good afternoon');
+} else if (hour > 18 && hour <= 24) {
+  console.log('Good evening');
+} else {
+  console.log('Good night');
+}
 
 
-// // let user = 'John', age = 25, message = 'Hello';
-// // console.log(user, age, message);
 
-// num = 5;
+let value = 17;
 
-// const myBirthday = '10.11.1990';
+if (typeof value === 'string') {
+  console.log('String');
+} else if (typeof value === 'number') {
+  console.log('Number');
+} else if (typeof value === 'boolean') {
+  console.log('Boolean');
+} else if (typeof value === 'undefined') {
+  console.log('Undefined');
+} else {
+  console.log('Undefined');
+}
 
-// // const myBirthday = '10.11.1991';
-// console.log(myBirthday);
-
-// const arr = [1,2,3,4,5];
-
-// const obj = {
-//   name: 'John',
-//   age: 25,
-//   0: 1,
-//   c: true
-// };
-
-// console.log(obj[c]);
-
-// console.log(arr[1]);  
+let x = 100;
 
 
-// let incr = 10;
-//   decr = 10;
+(x >= 10 && x <= 50) ? console.log('in range') : console.log('not in range');
 
-// // incr++;
-// // decr--;
 
-// console.log(incr++, decr--);
 
-// console.log(5%2);
+let num = 17;
 
-// let answer = prompt('Are you here?', 'yes');
-// console.log(typeof answer);
+if (num % 5 == 0 && num % 3 == 0) {
+  console.log('FizzBuzz');
+} else if (num % 3 == 0) {
+  console.log('Fizz');
+} else if (num % 5 == 0) {
+  console.log('Buzz');
+} else {
+  console.log(num);
+}
+
+
+
+const numberOne = 18000000;
+const numberTwo = 111000;
+
+if (numberOne - numberTwo > 0) {
+  console.log(`Число ${numberOne} больше ${numberTwo}`);
+} else {
+  console.log(`Число ${numberOne} меньше ${numberTwo}`);
+}
+
+
+let user = 'John', age = 25, message = 'Hello';
+console.log(user, age, message);
+
+num = 5;
+
+const myBirthday = '10.11.1990';
+
+const myBirthday = '10.11.1991';
+console.log(myBirthday);
+
+const arr = [1,2,3,4,5];
+
+const obj = {
+  name: 'John',
+  age: 25,
+  0: 1,
+  c: true
+};
+
+console.log(obj[c]);
+
+console.log(arr[1]);
+
+
+let incr = 10;
+  decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr++, decr--);
+
+console.log(5%2);
+
+let answer = prompt('Are you here?', 'yes');
+console.log(typeof answer);
 
 const questions = [];
 
@@ -52,3 +129,60 @@ let obj = {
 
 questions[4] = obj;
 console.log(questions);
+
+
+console.log(NaN || 2 || undefined);
+
+
+console.log(NaN && 2 && undefined);
+
+
+console.log(1 && 2 && 3);
+
+
+console.log(!1 && 2 || !3);
+
+
+console.log(25 || null && !3);
+
+
+console.log(NaN || null || !3 || undefined || 5);
+
+
+console.log(NaN || null && !3 && undefined || 5);
+
+
+console.log(5 === 5 && 3 > 1 || 5);
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+  console.log('Done!')
+}
+
+  let hamburger;
+  const fries = NaN;
+  const cola = 0;
+  const nuggets = 2;
+
+
+  if (hamburger || cola || fries === 3 || nuggets) {
+    console.log('Done!')
+  };
+
+
+  let hamburger = 1;
+  const fries = NaN;
+  const cola = 1;
+  const nuggets = 2;
+
+
+  if (hamburger && cola || fries === 3 && nuggets) {
+    console.log('Done!')
+  }
+ */
