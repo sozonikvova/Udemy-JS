d2b68d9155e6cebc5dddc393079c5adf5e87b947
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-08-22 10:24:46 +0300

    init: start app

--- js/script.js
+++ js/script.js
@@ -1,54 +1,26 @@
-'strict mode';
+'use strict';
+/* Задание на урок:
+
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
 
 
-// // let user = 'John', age = 25, message = 'Hello';
-// // console.log(user, age, message);
 
-// num = 5;
-
-// const myBirthday = '10.11.1990';
-
-// // const myBirthday = '10.11.1991';
-// console.log(myBirthday);
-
-// const arr = [1,2,3,4,5];
-
-// const obj = {
-//   name: 'John',
-//   age: 25,
-//   0: 1,
-//   c: true
-// };
-
-// console.log(obj[c]);
-
-// console.log(arr[1]);  
-
-
-// let incr = 10;
-//   decr = 10;
-
-// // incr++;
-// // decr--;
-
-// console.log(incr++, decr--);
-
-// console.log(5%2);
-
-// let answer = prompt('Are you here?', 'yes');
-// console.log(typeof answer);
-
-const questions = [];
-
-questions[0] = prompt('Are you here?', 'yes');
-questions[1] = +prompt('How old are you?', '5');
-questions[2] = prompt('Where are you from?', 'city');
-questions[3] = prompt('What is your name?', 'name');
-
-let obj = {
-  name: 'Vova',
-  age: 25,
-}
-
-questions[4] = obj;
-console.log(questions);
