d663ca5b895b587bded837c0b7f17c2b4d8d5ec8
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-08-22 10:43:29 +0300

    feat: add first section

--- js/script.js
+++ js/script.js
@@ -22,5 +22,21 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '5');
 
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};  
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
