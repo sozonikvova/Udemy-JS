adec64f95a27e3c49c0457c6ef26d8b493187ef8
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-09-02 12:40:43 +0300

    next: fucnction

--- js/script.js
+++ js/script.js
@@ -26,31 +26,47 @@ const personalMovieDB = {
 };
 
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = +prompt('На сколько оцените его?', '');
-  personalMovieDB.movies[a] = b;
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = +prompt('На сколько оцените его?', '');
+//   personalMovieDB.movies[a] = b;
 
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('Done');
+//   }
+//   else {
+//     console.log('Error');
+//     i--;
+//   }
+// }
+
+let i = 0;
+
+while (i < 2) {
+  const a = prompt('Один из просмотреных фильмов?', ''),
+    b = +prompt('Нас сколько вы его оцените?')
+  personalMovieDB.movies[a] = b;
+  i++;
   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
     personalMovieDB.movies[a] = b;
     console.log('Done');
   }
-  else {
-    console.log('Error');
-    i--;
-  }
 }
-
-if (personalMovieDB.count < 10) {
-  console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-  console.log('Вы киноман');
-} else {
-  console.log('Произошла ошибка');
-}
-
-
 
 console.log(personalMovieDB);
+
+
+
+//   if (personalMovieDB.count < 10) {
+//   console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//   console.log('Вы классический зритель');
+// } else if (personalMovieDB.count > 30) {
+//   console.log('Вы киноман');
+// } else {
+//   console.log('Произошла ошибка');
+// }
+
+
+
