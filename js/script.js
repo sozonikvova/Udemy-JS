331577cdd0a89da4699068f21bc03500bfafe666
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-09-05 12:46:15 +0300

    feat: add fucntions to the app

--- js/script.js
+++ js/script.js
@@ -36,59 +36,56 @@ const personalMovieDB = {
 };
 
 
-// for (let i = 0; i < 2; i++) {
-//   const a = prompt('Один из последних просмотренных фильмов?', ''),
-//     b = +prompt('На сколько оцените его?', '');
-//   personalMovieDB.movies[a] = b;
-
-//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//     personalMovieDB.movies[a] = b;
-//     console.log('Done');
-//   }
-//   else {
-//     console.log('Error');
-//     i--;
-//   }
-// }
-
-let i = 0;
-
-while (i < 2) {
-  const a = prompt('Один из просмотреных фильмов?', ''),
-    b = +prompt('Нас сколько вы его оцените?')
-  personalMovieDB.movies[a] = b;
-  i++;
-  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', '');
     personalMovieDB.movies[a] = b;
-    console.log('Done');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('Done');
+    }
+    else {
+      console.log('Error');
+      i--;
+    }
   }
 }
 
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
+}
+
+detectPersonalLevel();
+
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+
+writeYourGenres();
+
 console.log(personalMovieDB);
-
-
-
-//   if (personalMovieDB.count < 10) {
-//   console.log('Просмотрено довольно мало фильмов');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//   console.log('Вы классический зритель');
-// } else if (personalMovieDB.count > 30) {
-//   console.log('Вы киноман');
-// } else {
-//   console.log('Произошла ошибка');
-// }
-
-
-/* Задание на урок:
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
 
