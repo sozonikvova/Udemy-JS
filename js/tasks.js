bde9489c8201c716ab0eec7635ccdb1897a88722
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-09-10 13:56:43 +0300

    feat: variables types

---
+++ js/tasks.js
@@ -0,0 +1,20 @@
+'use strict';
+let number = 5;
+const leftBorderWith = 1;
+const obj = {
+  a: 50,
+}
+
+var name = 'Ivan';
+
+{
+  let result  = 50;
+
+}
+
+console.log(result);
+
+
+a = 50;
+
+console.log(a);
