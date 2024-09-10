e3ae7a6c678fbb7b325553dc35573163ab445404
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-09-10 15:05:38 +0300

    feat: operators in JS

--- js/tasks.js
+++ js/tasks.js
@@ -1,9 +1,45 @@
 'use strict';
 
-let category = 'toys';
+// strings
+console.log('string' + 'string'); // 'stringstring'
+// strings with numbers
+console.log('string' + 123); // 'string123'
+// numbers with strings
+console.log(123 + 'string'); // '123string'
+// numbers with numbers
+console.log(123 + 456); // 579
+// boolean with strings
+console.log(true + 'string'); // 'truestring'
+// strings with booleans
+console.log('string' + true); // 'stringtrue'
+// null with strings
+console.log(null + 'string'); // 'nullstring'
+// strings with null
+console.log('string' + null); // 'stringnull'
+// undefined with strings
+console.log(undefined + 'string'); // 'undefinedstring'
+// strings with undefined
+console.log('string' + undefined); // 'stringundefined'
+// objects with strings
+console.log({} + 'string'); // '[object Object]string'
+// strings with objects
+console.log('string' + {}); // 'string[object Object]'
+// arrays with strings
+console.log([] + 'string'); // 'string'
+// strings with arrays
+console.log('string' + []); // 'string'
 
-console.log(`https://someurl.com/${category}/5`); 
 
-const user = "Ivan";
+console.log(4 + +"5");
 
-alert(`Hello, ${user}`);
+let incr = 10,
+  decr = 11;
+
+console.log(incr++);
+
+console.log(5 % 2);
+
+const isChecked = false,
+  isClose = false;
+
+  console.log(!isChecked || !isClose);
