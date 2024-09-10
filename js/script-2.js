4c52e1ab8846df874a049a18f7a9ae03b6db38ed
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-09-10 13:45:12 +0300

    new: restart course

--- js/script-2.js
+++ js/script-2.js
@@ -1,11 +1,1 @@
 'use strict';
-
-const str = 'test';
-const arr = [1, 2, 3];
-console.log(str.slice(0, 1));
-console.log(arr.length);
-
-arr = [1, 2, 3];
-
-arr.push(4, 'Vova', 6, 8, 9, 10);
-console.log(arr);
