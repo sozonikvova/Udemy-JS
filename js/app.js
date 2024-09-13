23e6a136ee6c1d528af7327f37fcef44c73a7784
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-09-13 14:14:03 +0300

    feat: implement loop in app

--- js/app.js
+++ js/app.js
@@ -10,12 +10,27 @@ const personalMovieDB = {
   privat: false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = +prompt('На сколько оцените его?', ''),
-  с = prompt('Один из последних просмотренных фильмов?', ''),
-  d = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b; // запись в объект свойства movies первого фильма 
-personalMovieDB.movies[с] = d;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+};
 
 console.log(personalMovieDB);
+
+
