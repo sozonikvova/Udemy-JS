2ba899c841a1f14a5cb4033c78a7d451db342d73
Volodymyr Sozonik <anya.sozonik@gmail.com>
2024-09-13 10:45:10 +0300

    feat: tasks condition

--- js/tasks.js
+++ js/tasks.js
@@ -1,25 +1,75 @@
-if (4 == 9) {
-  console.log('ok');
+'use strict';
+
+const hamburger = 3;
+const fries = 1;
+
+if (hamburger && fries) {
+  console.log('I eat everything');
+};
+
+console.log((hamburger || fries));
+
+
+if (hamburger === 3 && fries) {
+  console.log('Everything is ok');
 } else {
-  console.log('error');
+  console.log('I eat something');
 }
 
-let num = 50;
-(num === 50) ? console.log('Ok') : console.log('Error');
+console.log(NaN || 2 || undefined);
 
 
-let numb = 49;
+console.log(NaN && 2 && undefined);
 
-switch (numb) {
-  case 49:
-    console.log('Ok');
-    break;
-  case 100:
-    console.log('Ok');
-    break;
-  case 50:
-    console.log('Ok');
-    break;
-  default:
-    console.log('Error');
+
+console.log(1 && 2 && 3);
+
+
+console.log(!1 && 2 || !3);
+
+
+console.log(25 || null && !3);
+
+
+console.log(NaN || null || !3 || undefined || 5);
+
+
+console.log(NaN || null && !3 && undefined || 5);
+
+
+console.log(5 === 5 && 3 > 1 || 5);
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//   console.log('Done!')
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//   console.log('Done!')
+// }
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+  console.log('Done!')
+} else {
+  console.log('Not done!')
 }
